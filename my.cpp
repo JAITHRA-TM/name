9eb9436b9c9cf22f7b8fa600fe167edb2469265a
JAITHRA-TM <jaithratm@gmail.com>
2023-11-27 20:28:12 +0530

    sui

--- my.cpp
+++ my.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
+
     int a = 10;
-    int b = 5;
-=======
-    int a = 11;
     int b = 4;
->>>>>>> 51f9cce5c8e61008d0a4e3d6a11797e3dbccdee4
     int c = 7;
-    cout<< a * b << endl;
+    cout<< a - b << endl;
 }
